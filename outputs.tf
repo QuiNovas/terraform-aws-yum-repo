4b76a89e9259aaf23189077fe2583d10785acf80
Joseph Wortmann <joseph.wortmann@gmail.com>
2018-08-22 17:03:11 -0500

    Added descriptions to variables and outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "website_endpoint" {
-  value = "${aws_s3_bucket.yum_repo.website_endpoint}"
+  description = "The endpoint of the created S3 bucket"
+  value       = "${aws_s3_bucket.yum_repo.website_endpoint}"
 }
